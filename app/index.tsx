25616a71a1ebe93bcabae97c9574e75074147908
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-02-12 00:06:17 +0530

    Added Bottom Sheet for Subscribe modal

--- app/index.tsx
+++ app/index.tsx
@@ -20,9 +20,12 @@ export default function Index() {
   const textColor = Colors[colorScheme ?? "light"].text;
   const subscribeModalRef = useRef<BottomSheetModal>(null);
 
+  const handlePresentSubscribeModal = () =>
+    subscribeModalRef.current?.present();
+
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      {/* <SubscribeModal ref={subscribeModalRef}/> */}
+      <SubscribeModal ref={subscribeModalRef} />
       <View style={styles.header}>
         <Icon width={100} height={100} />
         <ThemedText style={styles.title}>ShabDa</ThemedText>
@@ -46,11 +49,12 @@ export default function Index() {
             <ThemedText style={styles.btnText}>Login</ThemedText>
           </TouchableOpacity>
         </Link>
-        <Link href="/" style={[styles.btn, { borderColor: textColor }]} asChild>
-          <TouchableOpacity>
-            <ThemedText style={styles.btnText}>Subscribe</ThemedText>
-          </TouchableOpacity>
-        </Link>
+
+        <TouchableOpacity
+          onPress={handlePresentSubscribeModal}
+          style={[styles.btn, { borderColor: textColor }]}>
+          <ThemedText style={styles.btnText}>Subscribe</ThemedText>
+        </TouchableOpacity>
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerDate}>
--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,13 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { forwardRef, useCallback, useMemo } from "react";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  useBottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+export type Ref = BottomSheetModal;
 
-const SubscribeModal = () => {
-  return (
-    <View>
-      <Text>SubscribeModal</Text>
-    </View>
+const SubscribeModal = forwardRef<Ref>((props, ref) => {
+  const snapPoints = useMemo(() => ["90%"], []);
+  const { dismiss } = useBottomSheetModal();
+  const { bottom } = useSafeAreaInsets();
+
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        opacity={0.7}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        {...props}
+        onPress={dismiss}
+      />
+    ),
+    []
   );
-};
+
+  return (
+    <BottomSheetModal
+      ref={ref}
+      index={0}
+      backdropComponent={renderBackdrop}
+      snapPoints={snapPoints}
+      handleComponent={null}>
+      <View>
+        <Text>Hi</Text>
+      </View>
+    </BottomSheetModal>
+  );
+});
 
 export default SubscribeModal;
 
