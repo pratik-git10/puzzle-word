2f0b24acc29c92725eebab2bbe604afcb89736f2
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-02-14 13:02:25 +0530

    Completed Game logic

--- app/game.tsx
+++ app/game.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, useColorScheme, View } from "react-native";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Colors } from "@/constants/Colors";
 import { Stack, useRouter } from "expo-router";
 import OnScreenKeyboard from "@/components/OnScreenKeyboard";
 import { Ionicons } from "@expo/vector-icons";
+import { allWords } from "@/utils/allWords";
+import { words } from "@/utils/targetWords";
 
 const ROWS = 6;
 
@@ -15,17 +17,119 @@ const Page = () => {
   const router = useRouter();
 
   const [rows, setRows] = useState<string[][]>(
-    new Array(ROWS).fill(new Array(5).fill("a"))
+    new Array(ROWS).fill(new Array(5).fill(""))
   );
   const [curRow, setCurRow] = useState(0);
-  const [curCol, setCurCol] = useState(0);
+  const [curCol, _setCurCol] = useState(0);
 
   const [greenLetters, setGreenLetters] = useState<string[]>([]);
   const [yellowLetters, setYellowLetters] = useState<string[]>([]);
   const [grayLetters, setGrayLetters] = useState<string[]>([]);
 
+  const [word, setWord] = useState<string>(
+    words[Math.floor(Math.random() * words.length)]
+  );
+
+  const wordLetters = word.split("");
+  console.log(word);
+
+  const colStateRef = useRef(curCol);
+  const setCurCol = (col: number) => {
+    colStateRef.current = col;
+    _setCurCol(col);
+  };
+
+  // add Key Function
   const addKey = (key: string) => {
     console.log("Addkey", key);
+    const newRows = [...rows.map((row) => [...row])];
+    if (key === "ENTER") {
+      checkWord();
+    } else if (key == "BACKSPACE") {
+      if (colStateRef.current === 0) {
+        newRows[curRow][0] = "";
+        setRows(newRows);
+        return;
+      }
+      newRows[curRow][colStateRef.current - 1] = "";
+      setCurCol(colStateRef.current - 1);
+      setRows(newRows);
+      return;
+    } else if (colStateRef.current >= newRows[curRow].length) {
+      return;
+    } else {
+      newRows[curRow][colStateRef.current] = key;
+      setRows(newRows);
+      setCurCol(colStateRef.current + 1);
+    }
+  };
+
+  // Check Word Fuction
+  const checkWord = () => {
+    const currentWord = rows[curRow].join("");
+
+    if (currentWord.length < word.length) {
+      console.log("Complete the word");
+      return;
+    }
+    if (!allWords.includes(currentWord)) {
+      console.log("not a word");
+      // return;
+    }
+    // if (currentWord === word) {
+    //   console.log("You Won");
+    // }
+
+    const newGreen: string[] = [];
+    const newYellow: string[] = [];
+    const newGray: string[] = [];
+
+    currentWord.split("").forEach((letter, index) => {
+      if (letter == wordLetters[index]) {
+        newGreen.push(letter);
+      } else if (wordLetters.includes(letter)) {
+        newYellow.push(letter);
+      } else {
+        newGray.push(letter);
+      }
+    });
+    setGreenLetters([...greenLetters, ...newGreen]);
+    setYellowLetters([...yellowLetters, ...newYellow]);
+    setGrayLetters([...grayLetters, ...newGray]);
+
+    setTimeout(() => {
+      if (currentWord === word) {
+        console.log("You won!");
+      } else if (curRow + 1 >= rows.length) {
+        console.log("game over");
+      }
+    }, 0);
+
+    setCurRow(curRow + 1);
+    setCurCol(0);
+  };
+
+  const getCellColor = (cell: string, rowIndex: number, cellIndex: number) => {
+    if (curRow > rowIndex) {
+      if (wordLetters[cellIndex] === cell) {
+        return Colors.light.green;
+      } else if (wordLetters.includes(cell)) {
+        return Colors.light.yellow;
+      } else {
+        return grayColor;
+      }
+    }
+    return "transparent";
+  };
+  const getBorderColor = (
+    cell: string,
+    rowIndex: number,
+    cellIndex: number
+  ) => {
+    if (curRow > rowIndex && cell !== "") {
+      return getCellColor(cell, rowIndex, cellIndex);
+    }
+    return Colors.light.gray;
   };
 
   return (
@@ -49,8 +153,24 @@ const Page = () => {
         {rows.map((row, rowIndex) => (
           <View style={styles.gameFieldRow} key={`row-${rowIndex}`}>
             {row.map((cell, cellIndex) => (
-              <View style={styles.cell} key={`cell-${rowIndex}-${cellIndex}`}>
-                <Text style={styles.cellText}>{cell}</Text>
+              <View
+                style={[
+                  styles.cell,
+                  {
+                    backgroundColor: getCellColor(cell, rowIndex, cellIndex),
+                    borderColor: getBorderColor(cell, rowIndex, cellIndex),
+                  },
+                ]}
+                key={`cell-${rowIndex}-${cellIndex}`}>
+                <Text
+                  style={[
+                    styles.cellText,
+                    {
+                      color: curRow > rowIndex ? "#fff" : textColor,
+                    },
+                  ]}>
+                  {cell}
+                </Text>
               </View>
             ))}
           </View>
@@ -92,7 +212,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 2,
+    borderRadius: 5,
   },
   cellText: {
     fontSize: 30,
